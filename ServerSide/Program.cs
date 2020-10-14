e9e91887b5c3bdf457918ff61a7a7300d4e384ca
edseds <edgars.cirulis@gmail.com>
2020-10-14 22:50:19 +0300

    Add project files.

---
+++ ServerSide/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    class Program
+    {
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            Server server = new Server()
+            {
+                Ports = {new ServerPort("localhost")}
+            };
+
+            try
+            {
+
+            }
+            catch (IOException)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
