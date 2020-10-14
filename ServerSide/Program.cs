aa6bb298f3238c564e795ba08a58bfb5b3c766a9
edseds <edgars.cirulis@gmail.com>
2020-10-14 23:17:01 +0300

    -Client -Server side changes

--- ServerSide/Program.cs
+++ ServerSide/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Grpc.Core;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServerSide
 {
